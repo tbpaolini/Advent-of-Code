cac8030e946e13b54c653798dd83d36876419d14
Tiago Becerra Paolini <tpaolini@gmail.com>
2021-12-11 17:05:01 -0300

    Finished Day 1 of 2016

--- 2016/Day 1/taxicab.py
+++ 2016/Day 1/taxicab.py
@@ -1,32 +1,38 @@
 from math import sin, cos, radians
 import numpy as np
+from itertools import product
 
-with open(r"C:\Users\Tiago\OneDrive\Documentos\Python\Projetos\Advent of code\2016\Day 1\input.txt", "rt") as file:
+with open("input.txt", "rt") as file:
     raw_line = file.readline().rstrip()
     values = [(value[0], int(value[1:])) for value in raw_line.split(", ")]
-
-# Part 1
 
 x = 0
 y = 0
 angle = 0
+history = []
+overlaps = []
 
 for rot, dist in values:
     if   rot == "R": angle += 90
     elif rot == "L": angle -= 90
     rad = radians(angle)
 
-    x += round(dist * cos(rad))
-    y += round(dist * sin(rad))
+    x_start = x
+    y_start = y
+    x_dir = round(cos(rad))
+    y_dir = round(sin(rad))
+
+    x += dist * x_dir
+    y += dist * y_dir
+    x_range = range(x_start, x, x_dir) if x_dir != 0 else (x_start,)
+    y_range = range(y_start, y, y_dir) if y_dir != 0 else (y_start,)
+
+    for coord in product(x_range, y_range):
+        if coord in history: overlaps += [coord]
+        history += [coord]
 
 total_distance = abs(x) + abs(y)
-print(total_distance)
+print(f"Part 1: {total_distance}")
 
-# Part 2
-
-x = 0
-y = 0
-angle = 0
-visited = {(0, 0)}
-hq_distance = None
-
+hq_distance = abs(overlaps[0][0]) + abs(overlaps[0][1])
+print(f"Part 2: {hq_distance}")
