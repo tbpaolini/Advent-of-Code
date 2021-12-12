9f1b72cacbb783836ca2494fcf0109a2e4278247
Tiago Becerra Paolini <tpaolini@gmail.com>
2021-12-12 19:21:43 -0300

    Archived unused functions that might be used at some point

--- 2021/Day 12/caves.py
+++ 2021/Day 12/caves.py
@@ -47,26 +47,6 @@ for point_a, point_b in nodes:
     cave_b = Cave(point_b)
     cave_a.add_joint(cave_b)
 
-def is_sublist(list1:list, list2:list) -> bool:
-    size = len(list1)
-    for i in range(len(list2) - len(list1) + 1):
-        if list2[i : size+i] == list1: return True
-    else:
-        return False
-
-def split_list(my_list:list, value) -> list:
-    result = []
-    start = 0
-    for end, elm in enumerate(my_list):
-        if elm == value:
-            my_slice = my_list[start:end]
-            if my_slice: result += [my_slice]
-            start = end+1
-    
-    remainder = my_list[start:]
-    if remainder: result += [my_list[start:]]
-
-    return result
 
 def find_path(caves:dict):
     paths = set()
