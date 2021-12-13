0862045e47328b7c8e0793f5a35380d792d24164
Tiago Becerra Paolini <tpaolini@gmail.com>
2021-12-13 01:05:38 -0300

    Finished Day 12 of 2021 (FINALLY =D)

--- 2021/Day 12/caves.py
+++ 2021/Day 12/caves.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Dict
 from copy import deepcopy
+from collections import Counter
 
 with open(r"C:\Users\Tiago\OneDrive\Documentos\Python\Projetos\Advent of code\2021\Day 12\input.txt", "rt") as file:
     nodes = []
@@ -28,71 +29,72 @@ class Cave():
 
             return self
     
-    def __eq__(self, obj:object) -> bool:
+    def __eq__(self, obj:object) -> bool:   # We can compare wheter two caves are the same
         return self.name == obj
     
-    def __hash__(self) -> int:
+    def __hash__(self) -> int:  # Each cave returns a unique hash so it can be used in a set or dictionary key
         return hash(self.name)
     
-    def __repr__(self) -> str:
+    def __repr__(self) -> str:  # User friendly representation on terminal
         return f"Cave('{self.name}')"
     
-    def __deepcopy__(self, memo=None):
+    def __deepcopy__(self, memo=None):  # Enable the deep copy feature
         return Cave(self.name)
     
-    def add_joint(self, node:Cave) -> None:
+    def add_joint(self, node:Cave) -> None:  # Join two caves together
         self.joints.add(node)
         node.joints.add(self)
 
+
+# Create and join caves
 for point_a, point_b in nodes:
     cave_a = Cave(point_a)
     cave_b = Cave(point_b)
     cave_a.add_joint(cave_b)
 
-
-def is_sublist(list1:list, list2:list) -> bool:
-    size = len(list1)
-    for i in range(len(list2) - len(list1) + 1):
-        if list2[i : size+i] == list1: return True
-    else:
-        return False
-
-def find_path(caves:dict):
+def find_path(caves:dict, extra_visit=False):
     paths = set()
-    max_visited = 0
+    visit_count = Counter()
 
-    def travel(position:Cave, my_path:list=[]):
-        nonlocal paths, max_visited
+    # Recursively search through the paths
+    def travel(position:Cave, my_path:list=[], has_extra_visit:bool=False, count:Counter=visit_count):
+        nonlocal paths
 
+        # Stop if we arrived at the start again
         if (position == "start") and (len(my_path) > 1): return
 
+        # Stop once we reach the end
         if position == "end":
             paths.add(tuple(my_path + [position]))
             return
         
-        my_exits = position.joints
+        # Count the visits on the current path
+        my_count = deepcopy(count)
+        if position.type == "small":
+            my_count.update((position,))
+            max_visits = 2 if has_extra_visit else 1
+            visit_number = my_count[position]
+            if visit_number == max_visits: has_extra_visit=False
+            if visit_number > max_visits: return    # Stop if we reached the maximum visits of the small cave
 
-        for next_cave in my_exits:
-            if (next_cave.type == "small") and (next_cave in my_path):
-                continue
-            
-            if len(my_path) > 1:
-                node = [my_path[-1], position]
-                if is_sublist(node, my_path):
-                    continue
+        # Try each possible exit
+        for next_cave in position.joints:
+            travel(next_cave, my_path + [position], has_extra_visit, my_count)
 
-            travel(next_cave, my_path + [position])
-
-    travel(caves["start"])
+    # Begin recursive search of all paths
+    travel(caves["start"], has_extra_visit=extra_visit)
     return paths
 
-routes = find_path(Cave.cave_system)
-print(f"Part 1: {len(routes)}")
+routes_p1 = find_path(Cave.cave_system)
+print(f"Part 1: {len(routes_p1)}")
 
-def print_paths(paths):
-    for path in paths:
-        text = ", ".join(cave.name for cave in path)
-        print(text)
+routes_p2 = find_path(Cave.cave_system, True)
+print(f"Part 2: {len(routes_p2)}")
+
+# def print_paths(paths):
+#     for path in paths:
+#         text = ", ".join(cave.name for cave in path)
+#         print(text)
 
 # Part 1:
 # 39444 - too high
