77d77df4221b9fe0a6b51a72f81e994a2039867b
Tiago Becerra Paolini <tpaolini@gmail.com>
2021-12-17 14:07:29 -0300

    Finished Day 16 of 2021

--- 2021/Day 16/decoder.py
+++ 2021/Day 16/decoder.py
@@ -1,14 +1,19 @@
 from collections import deque, namedtuple
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 from uuid import UUID, uuid4
+from operator import gt, lt, eq
+from math import prod
 
-Packet = namedtuple("Packet", "version type_id parent payload")
+Packet = namedtuple("Packet", "version type_id parent payload value depth")
 
 class Decoder():
 
     def __init__(self, data:str) -> None:
         
+        # Convert the hexadecimal text to bytes
         raw_bytes = bytes.fromhex(data)
+
+        # Pack into a queue all bits from those bytes
         self.bits = deque()
         for value in raw_bytes:
             self.bits.extend(
@@ -16,10 +21,28 @@ class Decoder():
                 for n in range(7, -1, -1)
             )
         
+        # Store the packets and associate an unique ID to each
         self.packets:Dict[UUID,Packet] = {}
+        
+        # Checksum of the packets versions
         self.version_sum = 0
+
+        # Operators for each packet type:
+        # True means that it takes an iterable
+        # False means that it takes individual parameters
+        Oper = namedtuple("Operator", "function takes_iter")
+        self.operators = {
+            0: Oper(sum, True),     # Sum of all subpackets
+            1: Oper(prod, True),    # Product of all subpackets
+            2: Oper(min, True),     # Minimum of all subpackets
+            3: Oper(max, True),     # Maximum of all subpackets
+            5: Oper(gt, False),     # subpacked_a > subpacket_b
+            6: Oper(lt, False),     # subpacked_a < subpacket_b
+            7: Oper(eq, False)      # subpacked_a == subpacket_b
+        }
     
     def next_bits(self, amount:int) -> int:
+        """Unpack an amount of bits from the queue."""
         output = 0
         for bit in range(amount):
             try:
@@ -30,18 +53,22 @@ class Decoder():
         
         return output
     
-    def parse_packet(self, parent:Packet=None) -> Tuple[Packet, int]:
+    def parse_packet(self, parent:Packet=None, depth:int=0) -> Tuple[Packet, int]:
+        """Get all the data from the packet and calculate its value."""
         
-        uuid = uuid4()
-        bit_count = 0
+        uuid = uuid4()  # Unique ID for the packet
+        bit_count = 0   # Length in bits of the packet
 
+        # Parse the header
         version = self.next_bits(3)
         type_id = self.next_bits(3)
         bit_count += 6
         self.version_sum += version
 
+        # If the packet contains a literal value
         if type_id == 4:
             
+            # Parse the value until the leading bit of the fragment is 0
             payload = 0
             separator = 1
             while separator == 1:
@@ -49,39 +76,63 @@ class Decoder():
                 payload <<= 4
                 payload |= self.next_bits(4)
                 bit_count += 5
+            value = payload
         
+        # If the packet is an operator
         else:
+            
+            # Get the logic of the length header
             length_type = self.next_bits(1)
             bit_count += 1
-            payload = []
+            payload:List[Packet] = []
             
+            # When the length is the bit length of all subpackets
             if length_type == 0:
                 max_bits = self.next_bits(15)
                 bit_count += 15
                 decoded_size = 0
+                
+                # Parse subpackets until the length is reached
                 while decoded_size < max_bits:
-                    subpacket, packet_size = self.parse_packet(parent=uuid)
+                    subpacket, packet_size = self.parse_packet(parent=uuid, depth=depth+1)
                     payload += [subpacket]
                     decoded_size += packet_size
                     bit_count += packet_size
                 assert decoded_size == max_bits
             
+            # When the length is the amount of subpackets
             elif length_type == 1:
                 max_packets = self.next_bits(11)
                 bit_count += 11
+
+                # Parse subpackets until the length is reached
                 for _ in range(max_packets):
-                    subpacket, packet_size = self.parse_packet(parent=uuid)
+                    subpacket, packet_size = self.parse_packet(parent=uuid, depth=depth+1)
                     payload += [subpacket]
                     bit_count += packet_size
+            
+            # Determine the operation of the packet and its operands
+            my_oper = self.operators[type_id]
+            sub_values = (packet.value for packet in payload)
+            
+            # Perform the operation on the operands
+            if my_oper.takes_iter:
+                value = my_oper.function(sub_values)
+            else:
+                value = my_oper.function(*sub_values)
         
-        packet = Packet(version, type_id, parent, payload)
-        assert uuid not in self.packets
+        # Store the packet data in a Packet object
+        packet = Packet(version, type_id, parent, payload, value, depth)
+        assert uuid not in self.packets  # Yes, I do know that I am being overzealous here :-)
         self.packets[uuid] = packet
+
+        # Return the Packet object and its bit length
         return (packet, bit_count)
     
     def decode(self) -> None:
+        """Decode the values of all packets in the data stream."""
         
-        while len(self.bits) > 10:
+        while len(self.bits) > 10:  # A packet contain a minimum of 11 bits (6 for the header, 5 for the payload of a literal)
             try:
                 self.parse_packet()
             except StopDecoding:
@@ -94,10 +145,13 @@ class StopDecoding(Exception):
 
 if __name__ == "__main__":
 
-    with open(r"C:\Users\Tiago\OneDrive\Documentos\Python\Projetos\Advent of code\2021\Day 16\input.txt", "rt") as file:
+    with open("input.txt", "rt") as file:
         raw_text = file.read().strip()
     
     transmission = Decoder(raw_text)
     transmission.decode()
     
     print(f"Part 1: {transmission.version_sum}")
+
+    outer_value, = (packet.value for packet in transmission.packets.values() if packet.depth == 0)
+    print(f"Part 2: {outer_value}")
