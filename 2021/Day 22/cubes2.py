42e7a186befb772f87d6526ccd7e033787e49fd4
Tiago Becerra Paolini <tpaolini@gmail.com>
2021-12-24 19:18:30 -0300

    [Day 22 of 2021] Should be correct this time

--- 2021/Day 22/cubes2.py
+++ 2021/Day 22/cubes2.py
@@ -46,11 +46,23 @@ for turn, new in instructions:
             new_core += [Cuboid(Coordinate(old.min.x, old.min.y, old.min.z), Coordinate(new.min.x-1, old.max.y, old.max.z))]
             old = Cuboid(Coordinate(new.min.x, old.min.y, old.min.z), Coordinate(old.max.x, old.max.y, old.max.z))
 
-        if old.min.x <= new.min.x <= old.max.x:  # Negative direction
-            new_core += [Cuboid(Coordinate(old.min.x, old.min.y, old.min.z), Coordinate(new.min.x-1, old.max.y, old.max.z))]
-            old = Cuboid(Coordinate(old.max.x, old.min.y, old.min.z), Coordinate(new.min.x, old.max.y, old.max.z))
+        # y-axis
+        if old.min.x <= new.max.x <= old.max.x:  # Positive direction
+            new_core += [Cuboid(Coordinate(old.min.x, new.max.y+1, old.min.z), Coordinate(old.max.x, old.max.y, old.max.z))]
+            old = Cuboid(Coordinate(old.min.x, old.min.y, old.min.z), Coordinate(old.max.x, new.max.y, old.max.z))
 
-        
+        if old.min.x <= new.min.x <= old.max.x:  # Negative direction
+            new_core += [Cuboid(Coordinate(old.min.x, old.min.y, old.min.z), Coordinate(old.max.x, new.min.y-1, old.max.z))]
+            old = Cuboid(Coordinate(old.min.x, new.min.y, old.min.z), Coordinate(old.max.x, old.max.y, old.max.z))
+
+        # z-axis
+        if old.min.x <= new.max.x <= old.max.x:  # Positive direction
+            new_core += [Cuboid(Coordinate(old.min.x, old.min.y, new.max.z+1), Coordinate(old.max.x, old.max.y, old.max.z))]
+            old = Cuboid(Coordinate(old.min.x, old.min.y, old.min.z), Coordinate(old.max.x, old.max.y, new.max.z))
+
+        if old.min.x <= new.min.x <= old.max.x:  # Negative direction
+            new_core += [Cuboid(Coordinate(old.min.x, old.min.y, old.min.z), Coordinate(old.max.x, old.max.y, new.min.z-1))]
+            old = Cuboid(Coordinate(old.min.x, old.min.y, new.min.z), Coordinate(old.max.x, old.max.y, old.max.z))
     
     if turn == "on": new_core += [new]
     else: assert turn == "off"
