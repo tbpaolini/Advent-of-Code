8779a18313f58520abf40bf3bc2328ce1ae1f563
Tiago Becerra Paolini <tpaolini@gmail.com>
2021-12-26 20:10:53 -0300

    More progress of Day 15 of 2021

--- 2021/Day 15/chiton.py
+++ 2021/Day 15/chiton.py
@@ -2,20 +2,26 @@ from __future__ import annotations
 from collections import namedtuple
 from heapq import heappop, heappush
 from time import perf_counter
+import numpy as np
 
 Node = namedtuple("Node", "x y")
 Path = namedtuple("Path", "risk node")
 
-with open(r"C:\Users\Tiago\OneDrive\Documentos\Python\Projetos\Advent of code\2021\Day 15\input.txt", "rt") as file:
+with open(r"C:\Users\Tiago\OneDrive\Documentos\Python\Projetos\Advent of code\2021\Day 15\test_input.txt", "rt") as file:
     cave_map = []
     for line in file: cave_map.append([int(char) for char in line.rstrip()])
+    cave_map = np.array(cave_map)
 
-def path_find(cave_map:list[list[int]], start:Node, goal:Node) -> int:
+def path_find(cave_map:np.ndarray, start:Node, goal:Node) -> int:
     """Implementation of the A* algorithm to find the least risk path between
     two nodes in the cave"""
 
+    # Upper boundary of the map
+    max_y = len(cave_map) - 1
+    max_x = len(cave_map[0]) - 1
+
     # Risk of the starting position
-    start_risk = cave_map[start.y][start.x]
+    start_risk = cave_map[start.y, start.x]
     
     # Nodes that are known but weren't visited yet,
     # and their minimum risk to get there
@@ -55,11 +61,32 @@ def path_find(cave_map:list[list[int]], start:Node, goal:Node) -> int:
 
 # Part 1
 
-max_y = len(cave_map) - 1
-max_x = len(cave_map[0]) - 1
-
 start_time_part1 = perf_counter()
-risk_part1 = path_find(cave_map, Node(0,0), Node(max_x, max_y))
+goal_y, goal_x = cave_map.shape
+risk_part1 = path_find(cave_map, Node(0,0), Node(goal_x-1, goal_y-1))
 total_time_part1 = perf_counter() - start_time_part1
 
 print(f"Part 1: {risk_part1} (took {total_time_part1:.1f} seconds)")
+
+# Part 2
+
+initial_map = cave_map.copy()
+cave_map_p2 = initial_map.copy()
+
+for row in range(1, 5):
+    new_values = (initial_map + row - 1) % 9 + 1
+    cave_map_p2 = np.append(cave_map_p2, new_values, axis=0)
+
+initial_map = cave_map_p2.copy()
+for column in range(1, 5):
+    new_values = (initial_map + column - 1) % 9 + 1
+    cave_map_p2 = np.append(cave_map_p2, new_values, axis=1)
+
+start_time_part2 = perf_counter()
+goal_y, goal_x = cave_map_p2.shape
+risk_part2 = path_find(cave_map_p2, Node(0,0), Node(goal_x-1, goal_y-1))
+total_time_part2 = perf_counter() - start_time_part2
+
+print(f"Part 2: {risk_part2} (took {total_time_part2:.1f} seconds)")
+
+print(f"{total_time_part2 / total_time_part1}")
