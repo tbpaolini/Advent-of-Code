ead8a843d7d2b8dece67ea73d416619f4bafd85c
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-09 17:58:06 -0300

    Day 9 of 2022: Testing

--- 2022/Day 9/day_09.c
+++ 2022/Day 9/day_09.c
@@ -45,15 +45,55 @@ static void parse_input(char *line, Coord2_i64 *output)
     }
 }
 
-void coord_add(Coord2_i64 *coord_a, Coord2_i64 *coord_b)
+void coord_add(Coord2_i64 *restrict coord_a, Coord2_i64 *restrict coord_b)
 {
     coord_a->x += coord_b->x;
     coord_a->y += coord_b->y;
 }
 
+static inline Coord2_i64 coord_diff(Coord2_i64 coord_a, Coord2_i64 coord_b)
+{
+    return (Coord2_i64){
+        coord_b.x - coord_a.x,
+        coord_b.y - coord_a.y
+    };
+}
+
+static inline int64_t coord_dist(Coord2_i64 coord_a, Coord2_i64 coord_b)
+{
+    return abs(coord_a.x - coord_b.x) + abs(coord_a.y - coord_b.y);
+}
+
+static inline int64_t get_sign(int64_t value)
+{
+    if (value > 0)
+    {
+        // Positive
+        return 1;
+    }
+    else if (value < 0)
+    {
+        // Negative
+        return -1;
+    }
+    else
+    {
+        // Zero
+        return 0;
+    }
+}
+
+static inline Coord2_i64 coord_dir(Coord2_i64 coord_a, Coord2_i64 coord_b)
+{
+    return (Coord2_i64){
+        get_sign(coord_b.x - coord_a.x),
+        get_sign(coord_b.y - coord_a.y)
+    };
+}
+
 int main(int argc, char **argv)
 {
-    FILE *input = fopen("input.txt", "rt");
+    FILE *input = fopen("test.txt", "rt");
     char line[8];
 
     Coord2_i64 pos   = {0, 0};                  // Current position
@@ -70,10 +110,6 @@ int main(int argc, char **argv)
     Coord2_i64 movements[move_count];
     memset(movements, 0, sizeof(movements));
 
-    // Array to store the positions of the rope's head
-    Coord2_i64 head_pos[move_count];
-    memset(head_pos, 0, sizeof(head_pos));
-
     // Parse the movements from the input
     
     for (size_t move_index = 0; move_index < move_count; move_index++)
@@ -86,7 +122,6 @@ int main(int argc, char **argv)
         
         // Move the rope's head
         coord_add(&pos, &delta);
-        head_pos[move_index] = pos;
 
         // Check the maximum and minimum coordinates
         if (pos.x > max.x) max.x = pos.x;
@@ -98,8 +133,8 @@ int main(int argc, char **argv)
     fclose(input);
     assert(max.x > min.x && max.y > min.y);
 
-    const int64_t width  = max.x - min.x;
-    const int64_t height = max.y - min.y;
+    const int64_t width  = abs(max.x - min.x) + 1;
+    const int64_t height = abs(max.y - min.y) + 1;
     bool locations[height][width];
     memset(locations, 0, sizeof(locations));
 
@@ -109,12 +144,75 @@ int main(int argc, char **argv)
         Coord2_i64 tail;
     } rope = {{abs(min.x), abs(min.y)}, {abs(min.x), abs(min.y)}};
 
-    locations[rope.head.y][rope.head.x] = true;
-    uint64_t locations_count = 1;
+    locations[rope.tail.y][rope.tail.x] = true;
 
     for (size_t i = 0; i < move_count; i++)
     {
         delta = movements[i];
+        Coord2_i64 head_destination = rope.head;
+        coord_add(&head_destination, &delta);
+
+        const int64_t head_distance = coord_dist(rope.head, head_destination);
+        Coord2_i64 head_direction = coord_dir(rope.head, head_destination);
+
+        for (size_t j = 0; j < head_distance; j++)
+        {
+            coord_add(&rope.head, &head_direction);
+            const int64_t tail_distance = coord_dist(rope.tail, rope.head);
+            Coord2_i64 tail_direction = coord_dir(rope.tail, rope.head);
+            
+            switch (tail_distance)
+            {
+                case 3: // Separated on the diagonal
+                    coord_add(&rope.tail, &tail_direction);
+                    break;
+                
+                case 2: // Diagonally adjascent or separated on one axis
+                    
+                    if (abs(tail_direction.x) == 1 && abs(tail_direction.y) == 1)
+                    {
+                        // Diagonally adjascent (do nothing)
+                    }
+                    else if (tail_direction.y == 1)
+                    {
+                        // Vertically separated (move up or down)
+                        rope.tail.y += tail_direction.y;
+                    }
+                    else
+                    {
+                        // Horizontally separated (move right or left)
+                        rope.tail.x += tail_direction.x;
+                    }
+                    
+                    break;
+                
+                case 1: // Vertically or horizontally adjascent
+                case 0: // Both at the same position
+                    // (do nothing)
+                    break;
+
+                default:
+                    fprintf(stderr, "Error: Head and tail got too far away.\n");
+                    abort();
+                    break;
+            }
+
+            // Update the positions where the rope has been
+            locations[rope.tail.y][rope.tail.x] = true;
+        }
     }
+    
+    FILE *output = fopen("output.txt", "wt");
+    for (int64_t y = height-1; y >= 0; y--)
+    {
+        for (int64_t x = 0; x < width; x++)
+        {
+            const char text = locations[y][x] ? '#' : '.';
+            fputc(text, output);
+        }
+
+        fputc('\n', output);
+    }
+    fclose(output);
     
 }
