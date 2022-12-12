6a8a7e3e2eacec67c261c7548672ce7ff299ff5b
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-12 10:37:39 -0300

    Day 12 of 2022: Data structure for the path

--- 2022/Day 12/day_12.c
+++ 2022/Day 12/day_12.c
@@ -6,6 +6,35 @@
 #include <ctype.h>
 #include <assert.h>
 
+typedef struct MountainNode
+{
+    struct MountainCoord coord;     // (x, y) coordinates of this node
+    uint64_t elevation;             // The cost to get here from the previous node
+    uint64_t total_cost;            // The total cost to get here from the start
+    struct MountainNode *exits[4];  // The nodes this one connects to: {right, top, left, down} (NULL means no exit on a direction)
+    bool visited;                   // Whether this node has been visited already
+} MountainNode;
+
+typedef struct MountainPath
+{
+    struct MountainCoord *start;    // Staring node
+    struct MountainCoord *end;      // Destination node
+    uint64_t steps;                 // How many steps the path has
+    uint64_t total_cost;            // The total cost of the path
+    struct
+    {
+        struct MountainNode *current;   // Current node on the path
+        struct MountainNode *next;      // Next node on the path
+    } path; // Path from the start to the end
+};
+
+typedef struct MountainCoord
+{
+    size_t x;
+    size_t y;
+} MountainCoord;
+
+
 int main(int argc, char **argv)
 {
     FILE *input = fopen("input.txt", "rt");
@@ -28,6 +57,12 @@ int main(int argc, char **argv)
             abort();
         }
     }
+
+    for (size_t i = 0; i < rows; i++)
+    {
+        /* code */
+    }
+    
     
     return 0;
 }
