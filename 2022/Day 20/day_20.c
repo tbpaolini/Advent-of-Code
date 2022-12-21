c81cfc2c83347b367facc75f5c36d649a2aedfdc
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-20 21:53:20 -0300

    Day 20 of 2022: Reworked the solution for using a linked list instead of an array

--- 2022/Day 20/day_20.c
+++ 2022/Day 20/day_20.c
@@ -8,16 +8,10 @@
 #include <assert.h>
 
 typedef struct MixValue {
-    int64_t number; // The value itself
-    int64_t index;  // Current index of the value on the array
+    int64_t number;             // The value itself
+    struct MixValue *next;      // Next value on the list
+    struct MixValue *previous;  // Previous value on the list
 } MixValue;
-
-static inline void swap_pointers(void **pointer_1, void **pointer_2)
-{
-    *pointer_1 = (void*)((ptrdiff_t)(*pointer_1) ^ (ptrdiff_t)(*pointer_2));
-    *pointer_2 = (void*)((ptrdiff_t)(*pointer_2) ^ (ptrdiff_t)(*pointer_1));
-    *pointer_1 = (void*)((ptrdiff_t)(*pointer_1) ^ (ptrdiff_t)(*pointer_2));
-}
 
 int main(int argc, char **argv)
 {
@@ -29,61 +23,28 @@ int main(int argc, char **argv)
     while (fgets(line, sizeof(line), input)) value_count++;
     rewind(input);
 
-    MixValue values[value_count];       // The input values in the original order
-    MixValue *encrypted[value_count];   // Values on the original order, as pointers
-    MixValue *decrypted[value_count];   // Values on the correct order, as pointers
+    MixValue values[value_count];   // Array of input values in the original order
+    MixValue *decrypted = NULL;     // Values on a linked list in the correct order
 
     int64_t num_index = 0;
     while (fgets(line, sizeof(line), input))
     {
         assert(isdigit(line[0]) || line[0] == '-');
-        values[num_index] = (MixValue){atol(line), num_index};
-        encrypted[num_index] = &values[num_index];
-        decrypted[num_index] = &values[num_index];
-        num_index++;
+        values[num_index++] = (MixValue){atol(line), NULL, NULL};
     }
 
     fclose(input);
 
-    for (int64_t i = 0; i < value_count; i++)
+    // Link the values to each other
+    for (size_t i = 0; i < value_count; i++)
     {
-        MixValue *value = &values[i];
+        size_t next_id = (i == value_count - 1) ? 0 : i+1;
+        size_t prev_id = (i == 0) ? value_count - 1 : i-1;
         
-        int64_t old_index = value->index;
-        int64_t new_index = (value->number + old_index) % value_count;
-        
-        if (new_index <= 0) new_index = value_count + new_index - 1;
-        if (new_index == old_index) continue;
-        
-        value->index = new_index;
+        values[i].next = &values[next_id];
+        values[i].previous = &values[prev_id];
 
-        // Direction that the value is being moved: +1 = right; -1 = left
-        int64_t dir = new_index > old_index ? +1 : -1;
-
-        for (int64_t j = old_index + dir; j != new_index; j += dir)
-        {
-            // The values between the new and old indexes are moved to the opposite direction
-            decrypted[j]->index -= dir;
-        }
-        
-        decrypted[new_index]->index -= dir;
-
-        if (dir == 1)
-        {
-            memmove(&decrypted[old_index], &decrypted[old_index+1], (new_index - old_index) * sizeof(MixValue*));
-        }
-        else
-        {
-            memmove(&decrypted[new_index], &decrypted[new_index+1], (old_index - new_index) * sizeof(MixValue*));
-        }
-
-        decrypted[new_index] = value;
-
-        for (size_t i = 0; i < value_count; i++)
-        {
-            printf("%ld, ", decrypted[i]->number);
-        }
-        printf("\n");
+        if (values[i].number == 0) decrypted = &values[i];
     }
 
     return 0;
