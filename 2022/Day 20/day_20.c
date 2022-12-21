c259acb39ae0cdc7cf35157511598feb69583635
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-21 11:16:08 -0300

    Day 20 of 2022: Refactored the solution

--- 2022/Day 20/day_20.c
+++ 2022/Day 20/day_20.c
@@ -7,69 +7,70 @@
 #include <stddef.h>
 #include <assert.h>
 
-typedef struct MixValue {
-    int64_t number;             // The value itself
-    struct MixValue *next;      // Next value on the list
-    struct MixValue *previous;  // Previous value on the list
-} MixValue;
 
-static MixValue* cycle(MixValue *val, size_t count)
-{
-    MixValue *res = val->next;
-    for (size_t i = 0; i < count-1; i++)
-    {
-        assert(res != val);
-        res = res->next;
-    }
-    return res;
-}
+// Circular doubly linked list
+// The elements are linked to previous and next elements,
+// while the "first" and "last" elements are linked to each other
+typedef struct CircularList {
+    int64_t number;                 // The value itself
+    struct CircularList *next;      // Next value on the list
+    struct CircularList *previous;  // Previous value on the list
+} CircularList;
 
+// Perform mixing on the values
+// Note: The function updates the pointers between the values, the array itself is not changed.
+//       The values are in a circular doubly linked list.
 static void mix(
-    size_t value_count,             // Amount of values
-    MixValue values[value_count],  // Array of values (in original order)
-    size_t mix_times                // How many times to mix
+    int64_t value_count,                // Amount of values
+    CircularList values[value_count],   // Array of list's nodes (in the original order of the values)
+    size_t mix_times                    // How many times to mix
 )
 {
-    for (int64_t i = 0; i < value_count; i++)
+    // Perform one or more rounds of mixing
+    for (size_t n = 0; n < mix_times; n++)
     {
-        // Set the head of the list to the current value
-        MixValue *value = &values[i];
-        MixValue *head = value;                             
-        
-        // How many spaces to move the value in the circular list
-        // Important: Since the other elements are being rotated around the current value,
-        // we are taking the modulo by the list lenght minus 1 (not the list lenght itself).
-        int64_t steps = value->number % (value_count - 1);
-
-        // Remove the head from the list
-        // (the previous element becomes the new head)
-        head->previous->next = head->next;
-        head->next->previous = head->previous;
-        head = head->previous;
-
-        // Rotate the list's head forwards or backwards
-        if (steps > 0)
+        // Loop through all values in the original order
+        for (int64_t i = 0; i < value_count; i++)
         {
-            // Positive value: forwards
-            for (int64_t j = 0; j < steps; j++)
-            {
-                head = head->next;
-            }
-        }
-        else if (steps < 0)
-        {
-            // Negative value: backwards
-            for (int64_t j = 0; j < -steps; j++)
-            {
-                head = head->previous;
-            }
-        }
+            // Set the head of the list to the current value
+            CircularList *value = &values[i];
+            CircularList *head = value;                             
+            
+            // How many spaces to move the value in the circular list
+            // IMPORTANT: Since the other elements are being rotated around the current value,
+            // we are taking the modulo by the list lenght minus 1 (not the list lenght itself).
+            int64_t steps = value->number % (value_count - 1);
 
-        // Insert the value after the current head
-        value->previous = head;
-        value->next = head->next;
-        head->next->previous = value;
-        head->next = value;
+            // Remove the head from the list
+            // (the previous element becomes the new head)
+            head->previous->next = head->next;
+            head->next->previous = head->previous;
+            head = head->previous;
+
+            // Rotate the list's head forwards or backwards
+            if (steps > 0)
+            {
+                // Positive value: forwards
+                for (int64_t j = 0; j < steps; j++)
+                {
+                    head = head->next;
+                }
+            }
+            else if (steps < 0)
+            {
+                // Negative value: backwards
+                for (int64_t j = 0; j < -steps; j++)
+                {
+                    head = head->previous;
+                }
+            }
+
+            // Insert the value after the current head
+            value->previous = head;
+            value->next = head->next;
+            head->next->previous = value;
+            head->next = value;
+        }
     }
 }
 
@@ -83,17 +84,23 @@ int main(int argc, char **argv)
     while (fgets(line, sizeof(line), input)) value_count++;
     rewind(input);
 
-    MixValue values[value_count];   // Array of input values in the original order
-    MixValue *decrypted = NULL;     // Values on a linked list in the correct order
+    CircularList values_p1[value_count];    // Array of input values in the original order (for Part 1)
+    CircularList *decrypted_p1 = NULL;      // Values on a circular linked list in the correct order (starting from 0)
 
+    // Insert the input values on the array
     int64_t num_index = 0;
     while (fgets(line, sizeof(line), input))
     {
         assert(isdigit(line[0]) || line[0] == '-');
-        values[num_index++] = (MixValue){atol(line), NULL, NULL};
+        values_p1[num_index++] = (CircularList){atol(line), NULL, NULL};
     }
 
     fclose(input);
+
+    // Create a copy of the array (for Part 2)
+    CircularList values_p2[value_count];
+    memcpy(values_p2, values_p1, sizeof(values_p2));
+    CircularList *decrypted_p2 = NULL;  // Values on a circular linked list in the correct order (starting from 0)
 
     // Link the values to each other
     for (int64_t i = 0; i < value_count; i++)
@@ -103,18 +110,25 @@ int main(int argc, char **argv)
         int64_t prev_id = (i == 0) ? value_count - 1 : i-1;
         
         // Link the current value to the next and the pevious
-        values[i].next = &values[next_id];
-        values[i].previous = &values[prev_id];
+        values_p1[i].next = &values_p1[next_id];
+        values_p1[i].previous = &values_p1[prev_id];
+        values_p2[i].next = &values_p2[next_id];
+        values_p2[i].previous = &values_p2[prev_id];
 
         // Starting point to read the decrypted values
-        if (values[i].number == 0) decrypted = &values[i];
+        if (values_p1[i].number == 0) decrypted_p1 = &values_p1[i];
+        if (values_p2[i].number == 0) decrypted_p2 = &values_p2[i];
     }
+
+    // Perform one round of mixing for Part 1
+    mix(value_count, values_p1, 1);
     
     int64_t solution_p1 = 0;
+    int64_t solution_p2 = 0;
     
     for (size_t i = 1; i <= 3; i++)
     {
-        MixValue *value_p1 = decrypted;
+        CircularList *value_p1 = decrypted_p1;
         
         for (size_t j = 0; j < (1000 * i) % value_count; j++)
         {
