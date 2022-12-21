0651076a9cd844b590b8afa38189d9acbde7f61e
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-20 22:16:13 -0300

    Day 20 of 2022: Trying to get the linked list to work

--- 2022/Day 20/day_20.c
+++ 2022/Day 20/day_20.c
@@ -38,13 +38,56 @@ int main(int argc, char **argv)
     // Link the values to each other
     for (size_t i = 0; i < value_count; i++)
     {
+        // Wrap the indexes around
         size_t next_id = (i == value_count - 1) ? 0 : i+1;
         size_t prev_id = (i == 0) ? value_count - 1 : i-1;
         
+        // Link the current value to the next and the pevious
         values[i].next = &values[next_id];
         values[i].previous = &values[prev_id];
 
+        // Starting point to read the decrypted values
         if (values[i].number == 0) decrypted = &values[i];
+    }
+
+    for (size_t i = 0; i < value_count; i++)
+    {
+        MixValue *value = &values[i];
+        MixValue *new_position = value;
+
+        // Navigate fowards or backwards on the list
+        if (value->number > 0)
+        {
+            // Positive value: fowards
+            for (int64_t j = 0; j < value->number; j++)
+            {
+                new_position = new_position->next;
+            }
+            
+            // Link the current value to the values before and after it
+            value->previous = new_position;
+            value->next = new_position->next;
+
+            // Link the values before and after to the current values
+            new_position->next->previous = value;
+            new_position->next = value;
+        }
+        else if (value->number < 0)
+        {
+            // Negative value: backwards
+            for (int64_t j = 0; j < -value->number; j++)
+            {
+                new_position = new_position->previous;
+            }
+
+            // Link the current value to the values before and after it
+            value->next = new_position;
+            value->previous = new_position->previous;
+
+            // Link the values before and after to the current values
+            new_position->previous->next = value;
+            new_position->previous = value;
+        }
     }
 
     return 0;
