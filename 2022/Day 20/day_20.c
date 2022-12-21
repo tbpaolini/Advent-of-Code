a7e095c9669aa2570ae81142e854feb2890b4d85
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-21 10:39:26 -0300

    Day 20 of 2022: Moved mixing to its own function

--- 2022/Day 20/day_20.c
+++ 2022/Day 20/day_20.c
@@ -24,45 +24,12 @@ static MixValue* cycle(MixValue *val, size_t count)
     return res;
 }
 
-int main(int argc, char **argv)
+static void mix(
+    size_t value_count,             // Amount of values
+    MixValue values[value_count],  // Array of values (in original order)
+    size_t mix_times                // How many times to mix
+)
 {
-    // Open the input file
-    FILE *input = fopen("input.txt", "rt");
-    char line[16];
-    
-    int64_t value_count = 0;
-    while (fgets(line, sizeof(line), input)) value_count++;
-    rewind(input);
-
-    MixValue values[value_count];   // Array of input values in the original order
-    MixValue *decrypted = NULL;     // Values on a linked list in the correct order
-
-    int64_t num_index = 0;
-    while (fgets(line, sizeof(line), input))
-    {
-        assert(isdigit(line[0]) || line[0] == '-');
-        values[num_index++] = (MixValue){atol(line), NULL, NULL};
-    }
-
-    fclose(input);
-
-    // Link the values to each other
-    for (int64_t i = 0; i < value_count; i++)
-    {
-        // Wrap the indexes around
-        int64_t next_id = (i == value_count - 1) ? 0 : i+1;
-        int64_t prev_id = (i == 0) ? value_count - 1 : i-1;
-        
-        // Link the current value to the next and the pevious
-        values[i].next = &values[next_id];
-        values[i].previous = &values[prev_id];
-
-        // Starting point to read the decrypted values
-        if (values[i].number == 0) decrypted = &values[i];
-    }
-
-    assert(decrypted == cycle(decrypted, value_count));
-
     for (int64_t i = 0; i < value_count; i++)
     {
         // Set the head of the list to the current value
@@ -103,10 +70,46 @@ int main(int argc, char **argv)
         value->next = head->next;
         head->next->previous = value;
         head->next = value;
+    }
+}
 
-        assert(decrypted == cycle(decrypted, value_count));
+int main(int argc, char **argv)
+{
+    // Open the input file
+    FILE *input = fopen("input.txt", "rt");
+    char line[16];
+    
+    int64_t value_count = 0;
+    while (fgets(line, sizeof(line), input)) value_count++;
+    rewind(input);
+
+    MixValue values[value_count];   // Array of input values in the original order
+    MixValue *decrypted = NULL;     // Values on a linked list in the correct order
+
+    int64_t num_index = 0;
+    while (fgets(line, sizeof(line), input))
+    {
+        assert(isdigit(line[0]) || line[0] == '-');
+        values[num_index++] = (MixValue){atol(line), NULL, NULL};
     }
 
+    fclose(input);
+
+    // Link the values to each other
+    for (int64_t i = 0; i < value_count; i++)
+    {
+        // Wrap the indexes around
+        int64_t next_id = (i == value_count - 1) ? 0 : i+1;
+        int64_t prev_id = (i == 0) ? value_count - 1 : i-1;
+        
+        // Link the current value to the next and the pevious
+        values[i].next = &values[next_id];
+        values[i].previous = &values[prev_id];
+
+        // Starting point to read the decrypted values
+        if (values[i].number == 0) decrypted = &values[i];
+    }
+    
     int64_t solution_p1 = 0;
     
     for (size_t i = 1; i <= 3; i++)
