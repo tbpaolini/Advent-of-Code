d8d1d9eb13a4333e2720b1bcb7a1a938177870b6
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-12-22 13:04:07 -0300

    Day 22 of 2022: Instructions parsing

--- 2022/Day 22/day_22.c
+++ 2022/Day 22/day_22.c
@@ -7,6 +7,21 @@
 #include <stddef.h>
 #include <assert.h>
 
+// A node on the board
+typedef struct BoardNode
+{
+    int64_t row;                // Row number
+    int64_t column;             // Column number
+    struct BoardNode *exit[4];  // Pointers to the nodes this one connects to: {right, down, left, top}
+} BoardNode;
+
+// An instruction for how to move on the board
+typedef struct BoardInstruction
+{
+    int64_t value;          // Distance to move or the direction to turn
+    enum {MOVE, TURN} type; // Whether to move or turn
+} BoardInstruction;
+
 int main(int argc, char **argv)
 {
     // Open the input file
@@ -15,6 +30,7 @@ int main(int argc, char **argv)
 
     int64_t width  = 0;
     int64_t height = 0;
+    int64_t nodes_count = 0;
 
     // Determine the width and height of the map
     while (next_char != '\n')
@@ -24,6 +40,7 @@ int main(int argc, char **argv)
         while (next_char != '\n')
         {
             char_count++;
+            if (next_char != ' ') nodes_count++;
             next_char = fgetc(input);
         }
         
@@ -36,9 +53,24 @@ int main(int argc, char **argv)
         next_char = fgetc(input);
     }
 
+    int64_t instructions_count = 1;
+    while ( (next_char = fgetc(input)) != EOF )
+    {
+        if (isupper(next_char)) instructions_count += 2;
+    }
+
     char temp_board[height][width];
     memset(temp_board, ' ', sizeof(temp_board));
     rewind(input);
+
+    BoardInstruction *instruction = (BoardInstruction*)calloc(instructions_count, sizeof(BoardInstruction));
+    BoardNode *board = (BoardNode*)calloc(nodes_count, sizeof(BoardNode));
+
+    if (!instruction || !board)
+    {
+        fprintf(stderr, "Error: No enough memory\n");
+        abort();
+    }
 
     int64_t x = 0;
     int64_t y = 0;
@@ -52,7 +84,46 @@ int main(int argc, char **argv)
         x = 0;
     }
 
-    next_char = fgetc(input);
+    fgetc(input);
+    
+    char buffer[21];
+    size_t buff_id = 0;
+    buffer[0] = '\0';
+    size_t inst_id = 0;
+
+    while (inst_id < instructions_count)
+    {
+        next_char = fgetc(input);
+        if (isdigit(next_char))
+        {
+            buffer[buff_id++] = next_char;
+        }
+        else if (next_char == 'L' || next_char == 'R' || next_char == '\n')
+        {
+            buffer[buff_id++] = '\0';
+            assert(buff_id <= sizeof(buffer));
+            instruction[inst_id++] = (BoardInstruction){atol(buffer), MOVE};
+            if (next_char == 'L')
+            {
+                instruction[inst_id++] = (BoardInstruction){-1, TURN};
+            }
+            else if (next_char == 'R')
+            {
+                instruction[inst_id++] = (BoardInstruction){+1, TURN};
+            }
+            buff_id = 0;
+            buffer[0] = '\0';
+        }
+        else
+        {
+            fprintf(stderr, "Error: Malformatted input file\n");
+            abort();
+        }
+        assert(buff_id <= sizeof(buffer));
+    }
+
+    free(instruction);
+    free(board);
     
     return 0;
 }
