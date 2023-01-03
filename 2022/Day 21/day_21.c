5ae03172e5eda23f2e3a83b3bb62c79f7fc179a4
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-01-03 16:47:14 -0300

    Day 21 of 2022: Began C implementation of solution

--- 2022/Day 21/day_21.c
+++ 2022/Day 21/day_21.c
@@ -5,7 +5,25 @@
 #include <string.h>
 #include <assert.h>
 
+typedef struct MathMonkey
+{
+    struct MathMonkey *others[2];
+    double number;
+    char name[5];
+    char operation;
+} MathMonkey;
+
 int main(int argc, char * argv)
 {
+    FILE *input = fopen("input.txt", "rt");
+    char line[32];
 
+    while (fgets(line, sizeof(line), input))
+    {
+        /* code */
+    }
+
+    fclose(input);
+    
+    return 0;
 }
