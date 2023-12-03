125029fa32d4a1484b40a305d99c3931179e7f45
Tiago Becerra Paolini <tpaolini@gmail.com>
2023-12-03 16:10:10 -0300

    Day 2 of 2023: Part 2 solved

--- 2023/Day 02/day_02.py
+++ 2023/Day 02/day_02.py
@@ -1,24 +1,33 @@
-from collections import Counter
+from math import prod
+
 games = []
 
+# Each game is broken into a list of dictionaries with the amounts of each cube
 with open("input.txt", "rt") as file:
     for line in file:
+        # Get each draw from the game
         game = line.strip().split(": ")[1].split("; ")
         for i in range(len(game)):
             game[i] = game[i].split(", ")
             draw = game[i]
+            
+            # Get each cube from the draw
             my_dict = {}
             for cube in draw:
                 count, color = cube.split(" ")
                 my_dict[color] = int(count)
             game[i] = my_dict
+        
         games.append(game)
 
+# Maximum counts for Part 1
 max_cube = {
     "red": 12,
     "green": 13,
     "blue": 14,
 }
+
+#---------- Part 1 ----------#
 
 solution_p1 = 0
 for n, game in enumerate(games, start=1):
@@ -27,6 +36,7 @@ for n, game in enumerate(games, start=1):
     for draw in game:
         for color, count in draw.items():
             if count > max_cube[color]:
+                # If a color count is greater than its maximum, then the game is invalid
                 valid = False
                 break
         if not valid: break
@@ -34,4 +44,24 @@ for n, game in enumerate(games, start=1):
     if valid:
         solution_p1 += n
 
-print(solution_p1)
+print("Part 1:", solution_p1)
+
+#---------- Part 2 ----------#
+
+solution_p2 = 0
+for game in games:
+    min_cube = {
+    "red": 0,
+    "green": 0,
+    "blue": 0,
+    }
+
+    for draw in game:
+        for color, count in draw.items():
+            if (min_cube[color] < count):
+                # The minimum amount of a color should be the greatest amount of that color among all draws
+                min_cube[color] = count
+    
+    solution_p2 += prod(min_cube.values())
+
+print("Part 2:", solution_p2)
